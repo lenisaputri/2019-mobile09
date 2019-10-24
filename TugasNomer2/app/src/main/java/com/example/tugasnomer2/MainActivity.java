371225bd9266d0897e056b3a4e573b01dfb3e980
lenisaputri <lenisaputri98@gmail.com>
2019-10-24 16:17:28 +0700

    Tugas 2 FIX

--- TugasNomer2/app/src/main/java/com/example/tugasnomer2/MainActivity.java
+++ TugasNomer2/app/src/main/java/com/example/tugasnomer2/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.tugasnomer2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.tugasnomer2.adapters.HewanAdapter;
+import com.example.tugasnomer2.models.Hewan;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvHewan;
+    List<Hewan> listHewan = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //menyambungkan rvSuperHero ke layout
+        rvHewan = findViewById(R.id.rvHewan);
+        //Membuat object hero
+        Hewan hewan = new Hewan("Bebek", this.getResources().getDrawable(R.drawable.bebek_hewan));
+        //menambahkan hero ke listSuperHero
+        listHewan.add(hewan);
+        //membuat object hero baru
+        hewan = new Hewan("Gajah", this.getResources().getDrawable(R.drawable.gajah_hewan));
+        //menambahkan hero ke listSuperHero
+        listHewan.add(hewan);
+        //Instansiasi Adapter
+        HewanAdapter hewanAdapter = new HewanAdapter(listHewan);
+        //set adapter dan layoutmanager
+        rvHewan.setAdapter(hewanAdapter);
+        rvHewan.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
     }
 }
